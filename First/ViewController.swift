96b8b562e208ead5c775e03284c90fa0c3e6566f
Eugen Blattner <eugen.blattner@mayflower.de>
2018-10-31 11:14:18 +0100

    Initial Commit

---
+++ First/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Eugen on 31.10.18.
+//  Copyright © 2018 Blanorama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
